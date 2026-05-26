86fc027f07a4bdb6cfb5435db843887590c58f8f
Ramchandra prasad <kramchandraprasad7@gmail.com>
2026-05-26 23:27:01 +0530

    new commit

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+        
+    return data
+
+
 @app.get("/")
 def home():
-    return {"message": "Hello"}
+    return {"message": "Patient management system API"}
   
 @app.get('/about')
 def about():
-    return {"message": "campusx is an education platform where you can learn AI/ML"}
+    return {"message": "A fully functional API to manage your patient records"}
+  
+  
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
