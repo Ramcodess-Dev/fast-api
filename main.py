907963a8b542e38bedb7447a862ab9654e7357bf
Ramchandra prasad <kramchandraprasad7@gmail.com>
2026-05-27 12:43:06 +0530

    sec commit

--- main.py
+++ main.py
@@ -22,3 +22,12 @@ def about():
 def view():
     data = load_data()
     return data
+
+
+@app.get('patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'patient not found'}
